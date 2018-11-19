47784a63cb91f556556ff8416d8aeb0c0b7dc996
EchoFoe <Aspirpd@gmail.com>
2018-11-19 15:53:45 +0300

    Update settings_prod.py

--- Studio/settings_prod.py
+++ Studio/settings_prod.py
@@ -6,7 +6,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'db1',
-        'USER': 'echofoe',
+        'USER': 'echorise',
         'PASSWORD': 'flocksfoe',
         'HOST': 'localhost',
         'PORT': '',
